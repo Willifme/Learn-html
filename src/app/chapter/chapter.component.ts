95e4536d0b39c15fad14179fa1521f67fc8c19ed
Willifme <penguinwithwaffle@gmail.com>
2016-11-11 10:38:59 +0000

    Add title

--- src/app/chapter/chapter.component.ts
+++ src/app/chapter/chapter.component.ts
@@ -1,13 +1,17 @@
 import { Component, Input } from '@angular/core';
 
 import { ParagraphComponent } from '../paragraph/paragraph.component';
+import { MarkdownService } from '../markdown.service';
 import { Chapter } from '../markdown';
 
 @Component({
   selector: 'content-chapter',
   templateUrl: './chapter.component.html',
-  styleUrls: ['./chapter.component.css']
+  styleUrls: ['./chapter.component.css'],
+  providers: [MarkdownService]
 })
 export class ChapterComponent {
   @Input() private chapter: Chapter; 
+  
+  constructor(private markdown: MarkdownService) {}
 }
